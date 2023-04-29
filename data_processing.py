c8e4d6289dc4c57b4dd56dd0911b0e3cb9f650d4
rakesh-shah <rakeshshah55846@gmail.com>
2023-04-29 17:55:38 +0530

    wq! Revert "sum"
    This reverts commit 4f127def84a48c4929ab7003b8ecc8ec312b6629.

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,2 @@
 print("adding all the data for data processing")
 print("one more")
-
-a =5
-b =6
-
-def sum(a,b):
-    c = a+b
-    return a
-
-
-print("added a new function which is not correct")
